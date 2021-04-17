5b6d8ee47630ef06635d736e223cfc51c06d4326
csnoboa <caique2noboa@gmail.com>
2021-04-17 11:01:28 -0300

    removing whitespaces and tags

--- demo_project/spiders/jokes.py
+++ demo_project/spiders/jokes.py
@@ -1,4 +1,6 @@
 import scrapy
+from demo_project.spiders.items import JokeItem
+from scrapy.loader import ItemLoader
 
 class JokeSpider(scrapy.Spider):
     name = 'jokes'
@@ -7,9 +9,10 @@ class JokeSpider(scrapy.Spider):
 
     def parse(self, response):
         for joke in response.xpath("//div[@class='jokes']"):
-            yield{
-                'joke_text' : joke.xpath(".//div[@class='joke-text']/p").extract_first()
-            }
+            l = ItemLoader(item=JokeItem(), selector=joke)
+            l.add_xpath('joke_text', ".//div[@class='joke-text']/p")
+
+            yield l.load_item()
 
         next_page= response.xpath("//li[@class='next']/a/@href").extract_first()
 
